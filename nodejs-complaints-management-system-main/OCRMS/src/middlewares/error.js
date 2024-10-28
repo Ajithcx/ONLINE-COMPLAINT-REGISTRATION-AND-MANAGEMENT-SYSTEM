a65d5be879887057c7db11d5eba80c2ab5ad06ae
Ajithcx <146551365+Ajithcx@users.noreply.github.com>
2024-10-28 19:19:51 +0530

    Delete nodejs-complaints-management-system-main/OCRMS directory

--- nodejs-complaints-management-system-main/OCRMS/src/middlewares/error.js
+++
@@ -1,7 +0,0 @@
-const winston = require('winston');
-
-// Error handler middleware
-module.exports = function(err, req, res, next){
-    winston.error(err.message, err);
-    res.status(500).send(err.message);
-}
--- nodejs-complaints-management-system-main/OCRMS/src/middlewares/teamleader.js
+++
@@ -1,8 +0,0 @@
-
-
-// TeamLeader middleware
-module.exports = function(req, res, next){
-    if(req.user.role !== 'teamLeader') return res.status(403).send('Acces denied!');
-
-    next();
-}
